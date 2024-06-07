5d694d0106c4b9cc47c74f153d541783a6e72391
hny227zxy <167734372+hny227zxy@users.noreply.github.com>
2024-06-07 20:18:31 +0800

    Add files via upload

---
+++ 2024.06.07.py
@@ -0,0 +1,16 @@
+f = open('python.text','r')
+content = f.readlines()
+print(f'第一行：{content}')
+
+content = f.readlines()
+print(f'第二行：{content}')
+# ['hello world\n','abcdefg\n','aaa\n','bbb\n','ccc']
+# print(content)
+
+for line in open("python.text","r"):
+    print(line)
+ #关闭文件
+f.close()
+
+
+#课后练习
